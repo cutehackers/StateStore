f7d5d6011192a555597554d46be366c1fc2748d2
monolith-junhyoung <junhyoung.lee@monolith.co.kr>
2024-01-11 16:20:59 +0900

    feat: added sample code

--- logaware/app/src/main/java/app/junhyounglee/logaware/MainActivity.kt
+++ logaware/app/src/main/java/app/junhyounglee/logaware/MainActivity.kt
@@ -3,10 +3,10 @@ package app.junhyounglee.logaware
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LogAware {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        wtf('MainActivity created!')
     }
 }
